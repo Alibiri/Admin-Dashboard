79ba5d65c8ac78feba68fb2145643db1c8ea80e5
AlibiriFlottando <alimayweather7@gmail.com>
2025-10-18 19:43:50 +0100

    creating page for sales

---
+++ app/sales/page.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React from 'react'
+import { motion } from 'framer-motion'
+import StateCard from '@/components/StateCard'
+import { CreditCard, DollarSign, ShoppingCart, TrendingUp } from 'lucide-react'
+
+const SalesPage = () => {
+    return (
+        <div className='flex-1 overflow-auto relative z-10'>
+            <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+                <motion.div
+                    initial={{opacity:0, y:20}}
+                    animate={{opacity:1, y:0}}
+                    transition={{duration:1}}
+                    className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
+                >
+                    <StateCard name="Total Revenue" icon={DollarSign} value="$42,300"/>
+                    <StateCard name="Avg. Order Value" icon={ShoppingCart} value="$78,78"/>
+                    <StateCard name="Total Sales" icon={CreditCard} value="$125,700"/>
+                    <StateCard name="Total Growth" icon={TrendingUp} value="$37.2%"/>
+                </motion.div>
+            </main>
+        </div>
+    )
+}
+
+export default SalesPage
